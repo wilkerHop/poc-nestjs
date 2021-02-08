5c9cdd24494a6138584423f6436bad714034b13d
Wilker Ribeiro <rekliw97@gmail.com>
2021-02-08 19:19:43 -0300

    refactor(users): using specific error classes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
 import {
+  BadRequestException,
   Body,
   Controller,
   Delete,
   Get,
   HttpCode,
-  HttpException,
   HttpStatus,
+  InternalServerErrorException,
+  NotFoundException,
   Param,
   Post,
   Put,
@@ -21,11 +23,11 @@ export class UsersController {
   @HttpCode(HttpStatus.CREATED)
   create(@Body() user: CreateUserDto) {
     return this.usersService.createUser(user).catch((e: Error) => {
-      if (e.message === 'Username already taken') {
-        throw new HttpException(e.message, HttpStatus.BAD_REQUEST);
+      if (e.message.includes('username_unique')) {
+        throw new BadRequestException('Username already taken');
       }
 
-      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new InternalServerErrorException(e.message);
     });
   }
 
@@ -38,10 +40,10 @@ export class UsersController {
   getUser(@Param('username') username: string) {
     return this.usersService.user({ username }).catch((e: Error) => {
       if (e.message === 'User not found') {
-        throw new HttpException(e.message, HttpStatus.NOT_FOUND);
+        throw new NotFoundException(e.message);
       }
 
-      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new InternalServerErrorException(e.message);
     });
   }
 
@@ -50,11 +52,15 @@ export class UsersController {
     return this.usersService
       .updateUser({ where: { username }, data })
       .catch((e: Error) => {
-        if (e.message === 'User not found') {
-          throw new HttpException(e.message, HttpStatus.NOT_FOUND);
+        if (e.message.includes('username_unique')) {
+          throw new BadRequestException('Username already taken');
         }
 
-        throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+        if (e.message === 'User not found') {
+          throw new NotFoundException(e.message);
+        }
+
+        throw new InternalServerErrorException(e.message);
       });
   }
 
@@ -63,10 +69,10 @@ export class UsersController {
   deleteUser(@Param('username') username: string) {
     return this.usersService.deleteUser({ username }).catch((e: Error) => {
       if (e.message === 'User not found') {
-        throw new HttpException(e.message, HttpStatus.NOT_FOUND);
+        throw new NotFoundException(e.message);
       }
 
-      throw new HttpException(e.message, HttpStatus.INTERNAL_SERVER_ERROR);
+      throw new InternalServerErrorException(e.message);
     });
   }
 }
