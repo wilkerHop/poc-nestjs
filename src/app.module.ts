6e5aad88bdab283e24b26387572c64bf15cfe6d3
Wilker Ribeiro <rekliw97@gmail.com>
2021-02-09 03:41:02 -0300

    fix(app): removes tweet module import for a while

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './users/user.module';
-import { TweetsModule } from './tweets/tweets.module';
+// import { TweetModule } from './tweets/tweet.module';
 
 @Module({
-  imports: [UserModule, TweetsModule],
+  imports: [UserModule /*,TweetModule*/],
   controllers: [AppController],
   providers: [AppService],
 })
