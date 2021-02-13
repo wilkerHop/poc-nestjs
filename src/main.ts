c9b2e98e424b3d250cedf557ebd9c3244cc3e60f
Wilker Ribeiro <rekliw97@gmail.com>
2021-02-13 08:47:18 -0300

    feat: implements helmet

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(helmet());
 
   await app.listen(3000);
 }
