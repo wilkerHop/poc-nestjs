9a899dc41b9c6875f51fb36d860a44e6f5cf02f4
Wilker Ribeiro <rekliw97@gmail.com>
2021-02-18 07:43:21 -0300

    chore: implements fabrica-sinapse-nodejs-template
    Created files:
        .eslintignore
        .gitattributes
        .prettierignore
    Added more rules to .eslintrc.js and .prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,17 +21,29 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    "no-console": [
+    'arrow-parens': 'off',
+    eqeqeq: 'error',
+    'function-paren-newline': 'off',
+    indent: ['error', 2],
+    'linebreak-style': [2, 'unix'],
+    'no-console': [
       1,
       {
-        "allow": [
-          "time",
-          "timeEnd",
-          "info",
-          "warn",
-          "error"
-        ]
-      }
+        allow: ['time', 'timeEnd', 'info', 'warn', 'error'],
+      },
     ],
+    'no-duplicate-imports': 'error',
+    'no-extra-parens': 'error',
+    'no-return-await': 'error',
+    'no-shadow': [
+      'error',
+      {
+        builtinGlobals: false,
+        hoist: 'functions',
+        allow: [],
+      },
+    ],
+    'operator-linebreak': [2, 'before', { overrides: { '?': 'after' } }],
+    'import/prefer-default-export': 'off',
   },
 };
